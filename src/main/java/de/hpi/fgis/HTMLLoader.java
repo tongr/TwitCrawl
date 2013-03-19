81ca7da41f11eea8e33b7f010e2de0d89cfd26c8
tongr <tongr@online>
2013-03-19 15:17:29 +0100

    exended example for storing web pages to mongodb

--- src/main/java/de/hpi/fgis/HTMLLoader.java
+++ src/main/java/de/hpi/fgis/HTMLLoader.java
@@ -1,12 +1,16 @@
 package de.hpi.fgis;
 
 import java.io.IOException;
+import java.util.Date;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
-import com.mongodb.util.JSON;
 
+import de.hpi.fgis.database.mongodb.MongoDBObjectManager;
 import de.hpi.fgis.html.WebPageExtractor;
+import de.hpi.fgis.json.ChainedTransformation;
+import de.hpi.fgis.json.ITransformation;
+import de.hpi.fgis.json.NullFilter;
 
 public class HTMLLoader {
 
@@ -14,19 +18,28 @@ public class HTMLLoader {
 	 * @param args
 	 * @throws IOException 
 	 */
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 		if(args.length<=0) {
-			throw new IllegalArgumentException("Please specify an URL o load!");
-		} 
-		//MongoDBObjectManager pageMan = new MongoDBObjectManager("webpages", false);
-		WebPageExtractor extractor = new WebPageExtractor();
+			throw new IllegalArgumentException("Please specify one or more URL(s) to be loaded!");
+		}
+		System.out.println(new Date().toGMTString());
+		MongoDBObjectManager pageMan = new MongoDBObjectManager("webpages", false);
+		
+		// extract web pages and ignore empty attribute values
+		ITransformation extractor = new ChainedTransformation()
+			.addTransformation(new WebPageExtractor())
+			.addTransformation(new NullFilter());
 		for(String url : args) {
 			System.out.println("extracting data from " + url);
 			
-			DBObject data = extractor.transform(new BasicDBObject("url", url));
-			System.out.println(JSON.serialize(data));
-			// TODO add data to mongo
+			DBObject pageData = extractor.transform(new BasicDBObject("url", url));
+			// add data to mongo
+			pageMan.store(pageData);
+			pageMan.commit();
 		}
+		pageMan.close();
+		System.out.println(new Date().toGMTString());
 	}
 
 }
