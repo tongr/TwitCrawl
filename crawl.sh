87aa691d70fecbac2c3daf19d65a6ef7114fa200
tongr <tongr@online>
2013-11-13 16:40:47 +0100

    merge

--- crawl.sh
+++ crawl.sh
@@ -4,5 +4,5 @@ java="/usr/java/latest/bin/java"
 
 #echo "$@" >> job.log
 echo "starting crawl of $@: $(date)" >> job.log
-$java -Xms1g -Xmx20g -classpath $cp de.hpi.fgis.YQLDumpFileCrawler "$@" >> progress.log 2> error.log
+$java -Xms1g -Xmx20g -classpath $cp de.hpi.fgis.YQLDumpFileCrawler "$@" >> progress.log 2>> error.log
 echo "crawl of $@ finished: $(date)" >> job.log
